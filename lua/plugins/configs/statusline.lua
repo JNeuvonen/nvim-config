33959c5121f1279a8aeca052e747b3174601582b
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-10-21 10:57:21 +0300

    Init commit

---
+++ lua/plugins/configs/statusline.lua
@@ -0,0 +1,9 @@
+require'lualine'.setup {
+  options = {
+    component_separators = {'|', '|'},
+    section_separators = {'', ''},
+    -- other options
+  },
+  -- other lualine configuration
+}
+
