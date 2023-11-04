ab2b6745d662c276d5c0c774e0a5bb8ec296d99e
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-11-04 10:52:21 +0200

    Add binding for Telescope lsp_document_symbols

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -193,13 +193,6 @@ M.lspconfig = {
       "LSP references",
     },
 
-    ["<leader>f"] = {
-      function()
-        vim.diagnostic.open_float { border = "rounded" }
-      end,
-      "Floating diagnostic",
-    },
-
     ["[d"] = {
       function()
         vim.diagnostic.goto_prev { float = { border = "rounded" } }
@@ -273,6 +266,7 @@ M.telescope = {
     ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "Find files" },
     ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
     ["<leader>fw"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
+    ["<leader>fs"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Find symbols from document" },
     ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
     ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Help page" },
     ["<leader>fo"] = { "<cmd> Telescope oldfiles <CR>", "Find oldfiles" },
