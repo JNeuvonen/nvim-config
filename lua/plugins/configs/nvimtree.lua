e771f904d5f63cc4fbaba3b28b8e138fca1b92d4
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-11-07 20:52:57 +0200

    Make custom nvimtree mappings

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -20,7 +20,7 @@ local options = {
   },
   git = {
     enable = false,
-    ignore = true,
+    ignore = false,
   },
   filesystem_watchers = {
     enable = true,
@@ -34,11 +34,9 @@ local options = {
     root_folder_label = false,
     highlight_git = false,
     highlight_opened_files = "none",
-
     indent_markers = {
       enable = false,
     },
-
     icons = {
       show = {
         file = true,
@@ -46,7 +44,6 @@ local options = {
         folder_arrow = true,
         git = false,
       },
-
       glyphs = {
         default = "󰈚",
         symlink = "",
@@ -73,5 +70,76 @@ local options = {
     },
   },
 }
-
+require("nvim-tree").setup(options)
+local function toggle_gitignore_filter()
+  require("nvim-tree.api").tree.toggle_gitignore_filter()
+end
+local function increase_tree_width()
+  local view = require "nvim-tree.view"
+  if view.is_visible() then
+    local current_width = view.View.width
+    local new_width = current_width + 10
+    vim.cmd("NvimTreeResize " .. new_width)
+  else
+    print "NvimTree is not open"
+  end
+end
+local function decrease_tree_width()
+  local view = require "nvim-tree.view"
+  if view.is_visible() then
+    local current_width = view.View.width
+    local new_width = current_width - 10
+    if new_width > 0 then
+      vim.cmd("NvimTreeResize " .. new_width)
+    else
+      print "NvimTree width cannot be reduced further"
+    end
+  else
+    print "NvimTree is not open"
+  end
+end
+local function reset_tree_width()
+  local view = require "nvim-tree.view"
+  if view.is_visible() then
+    local default_width = 30
+    vim.cmd("NvimTreeResize " .. default_width)
+  else
+    print "NvimTree is not open"
+  end
+end
+local function setup_keybindings()
+  vim.api.nvim_set_keymap("n", "<leader>ti", "Increase NvimTree size", {
+    noremap = true,
+    silent = true,
+    callback = increase_tree_width,
+  })
+  vim.api.nvim_set_keymap("n", "<leader>tr", "Reduce NvimTree size", {
+    noremap = true,
+    silent = true,
+    callback = decrease_tree_width,
+  })
+  vim.api.nvim_set_keymap("n", "<leader>tR", "Make NvimTree default size", {
+    noremap = true,
+    silent = true,
+    callback = reset_tree_width,
+  })
+  vim.api.nvim_set_keymap("n", "<leader>tg", "Toggle show/hide git ignored files", {
+    noremap = true,
+    silent = true,
+    callback = toggle_gitignore_filter,
+  })
+end
+local wk = require "which-key"
+wk.register {
+  ["<leader>"] = {
+    t = {
+      name = "NvimTree", -- group name for all NvimTree related keybindings
+      i = { increase_tree_width, "Increase NvimTree size" },
+      r = { decrease_tree_width, "Reduce NvimTree size" },
+      R = { reset_tree_width, "Make NvimTree default size" },
+      g = { toggle_gitignore_filter, "Toggle show/hide git ignored files" },
+    },
+  },
+}
+setup_keybindings()
 return options
